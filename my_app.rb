c3edd93efd724062130f59d2b1d78721d6c774b2
Ken Lu <ken@seeice.com>
2013-05-25 16:45:13 -0400

    changed classic style to Modular style to enable pry-debugger

--- my_app.rb
+++ my_app.rb
@@ -1,26 +1,29 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'rubygems'
 require 'pry'
 require 'ap'
 
-enable :sessions
 
-get '/' do
-  #debugger
-  binding.pry
-  session[:user] = 'anonymouse'
-  redirect to('/bar')
-end
+class MyApp < Sinatra::Base
+  enable :sessions
+  get '/' do
+    #debugger
+    binding.pry
+    session[:user] = 'anonymouse'
+    redirect to('/bar')
+  end
 
-get '/:who' do
-  pass unless params[:who] = 'Ken'
-  "You know me"
-end
+  get '/:who' do
+    pass unless params[:who] = 'Ken'
+    "You know me"
+  end
 
-get '/bar' do
-  session[:user]
-end
+  get '/bar' do
+    session[:user]
+  end
 
-get '/*' do
-  'Who you are'
+  get '/*' do
+    'Who you are'
+  end
+  run! if app_file == $0
 end
